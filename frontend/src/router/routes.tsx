72ffd15f56ce3f86397b786b1fe43857ade30d9e
Valdir Silva <valdipiresba@gmail.com>
2024-08-25 15:15:36 -0300

    feat: create controller access routes private

--- frontend/src/router/routes.tsx
+++ frontend/src/router/routes.tsx
@@ -1,31 +1,20 @@
-import { useEffect, useState } from "react"
 import { Login } from "../pages/login/login"
 import { Home } from "../pages/home/home"
-import { Routes, Route, useNavigate } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import { Company } from "../pages/company/company"
 import { Candidate } from "../pages/candidate/candidate"
 import { UserProfile } from "../pages/profile/user-profile"
+import { PrivateRoute } from "./private-route"
 
 export function Router() {
-    const navigate = useNavigate()
-    const [hasUser, setHasUser] = useState(false)
-
-    useEffect(() => {
-        (() => {
-            const user = localStorage.getItem("@Auth:token")
-            if (!user) return navigate("/")
-            setHasUser(true)
-        })()
-    }, [hasUser])
-
     return (
         <Routes>
             <Route path="/" element={<Login />} />
-            <Route path="/jobs" element={<Home />} />
-            <Route path="/candidate" element={<Candidate />} />
             <Route path="/company" element={<Company />} />
-
-            <Route path="/profile" element={<UserProfile />} />
+            <Route path="/candidate" element={<Candidate />} />
+            <Route path="/jobs" element={<PrivateRoute element={Home} />} />
+            <Route path="/profile" element={<PrivateRoute element={UserProfile} />} />
         </Routes>
     )
 }
+
