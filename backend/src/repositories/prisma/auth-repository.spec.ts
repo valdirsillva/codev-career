fe8d78d51087d505fc6ac9d4477f09a80f2d3ed1
Valdir Silva <valdipiresba@gmail.com>
2024-09-23 12:27:44 -0300

    test: retorna null para usuario nao encontrado

--- backend/src/repositories/prisma/auth-repository.spec.ts
+++ backend/src/repositories/prisma/auth-repository.spec.ts
@@ -14,11 +14,18 @@ describe('Auth Repository', () => {
       name: 'Test User',
       role: 'user'
     }
-
     const prismaRepositoryStub = sut()
     const spyAuth = jest.spyOn(prismaRepositoryStub, 'login').mockResolvedValue(makeFakeUser)
     const response = await prismaRepositoryStub.login({ email: 'jhondoe@gmail.com', password: '123' })
     expect(spyAuth).toHaveBeenCalledWith({ email: 'jhondoe@gmail.com', password: '123' })
     expect(response).toEqual(makeFakeUser)
+  })
+
+  test('Should return null if not user', async () => {
+    const prismaRepositoryStub = sut()
+    const spyAuth = jest.spyOn(prismaRepositoryStub, 'login').mockResolvedValue(null)
+    const response = await prismaRepositoryStub.login({ email: 'anyuser@gmail.com', password: '123' })
+    expect(spyAuth).toHaveBeenCalledWith({ email: 'anyuser@gmail.com', password: '123' })
+    expect(response).toBeNull()
   })
 })
