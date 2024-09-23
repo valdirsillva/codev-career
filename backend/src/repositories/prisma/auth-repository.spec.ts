cfe7a66068c48f04b5fbc18bdc4a9263ff67d833
Valdir Silva <valdipiresba@gmail.com>
2024-09-23 12:21:38 -0300

    refactor: criando factory method

--- backend/src/repositories/prisma/auth-repository.spec.ts
+++ backend/src/repositories/prisma/auth-repository.spec.ts
@@ -1,22 +1,24 @@
 import { PrismaAuthRepository } from "./prisma-auth-repository"
-import { prisma } from '../../views/lib/prisma'
+
+const sut = (): PrismaAuthRepository => {
+  return new PrismaAuthRepository()
+}
 
 describe('Auth Repository', () => {
-    test('Should return a user object on login', async () => {
-        const mockUser = {
-            id: '1',
-            email: 'teste@teste.com',
-            password: 'hashed_password',
-            password_reset: null,
-            name: 'Test User',
-            role: 'user'
-        }
+  test('Should return a user object on login', async () => {
+    const makeFakeUser = {
+      id: '1',
+      email: 'teste@teste.com',
+      password: 'hashed_password',
+      password_reset: null,
+      name: 'Test User',
+      role: 'user'
+    }
 
-        const prismaRepositoryStub = new PrismaAuthRepository()
-        const spyAuth = jest.spyOn(prismaRepositoryStub, 'login').mockResolvedValue(mockUser)
-        const result = await prismaRepositoryStub.login({ email: 'jhondoe@gmail.com', password: '123' })
-
-        expect(spyAuth).toHaveBeenCalledWith({ email: 'jhondoe@gmail.com', password: '123' })
-        expect(result).toEqual(mockUser)
-    })
+    const prismaRepositoryStub = sut()
+    const spyAuth = jest.spyOn(prismaRepositoryStub, 'login').mockResolvedValue(makeFakeUser)
+    const response = await prismaRepositoryStub.login({ email: 'jhondoe@gmail.com', password: '123' })
+    expect(spyAuth).toHaveBeenCalledWith({ email: 'jhondoe@gmail.com', password: '123' })
+    expect(response).toEqual(makeFakeUser)
+  })
 })
