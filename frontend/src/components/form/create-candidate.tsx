177553c4cff79a7f9456fa4246f349e53c7750d7
Valdir Silva <valdirpiresba@gmail.com>
2025-01-18 20:12:06 -0300

    feat: implementando tela de cadastro de candidatos

--- frontend/src/components/form/create-candidate.tsx
+++ frontend/src/components/form/create-candidate.tsx
@@ -1,103 +1,138 @@
+import { toast, ToastContainer } from "react-toastify"
 import { useState, ChangeEvent, FormEvent } from "react"
-import { ArrowLeft } from "lucide-react"
-import { Link } from "react-router-dom"
 import { makeServiceApi } from "../../factories/api-service-factory"
 
 interface Candidate {
-    name: string
-    email: string
-    password: string
+	name: string
+	email: string
+	password: string
+	cpf: string
+	phoneNumber: string
 }
 
 export function CreateCandidate() {
-    const [fieldValues, setFieldValues] = useState<Candidate>({
-        name: '',
-        email: '',
-        password: ''
-    })
+	const [fieldValues, setFieldValues] = useState<Candidate>({
+		name: '',
+		email: '',
+		password: '',
+		cpf: '',
+		phoneNumber: '',
+	})
 
-    const handleChangeValues = (e: ChangeEvent<HTMLInputElement>) => {
-        const fieldName = e.target.name
-        const fieldValue = e.target.value
+	const handleChangeValues = (e: ChangeEvent<HTMLInputElement>) => {
+		const fieldName = e.target.name
+		const fieldValue = e.target.value
 
-        setFieldValues((current) => {
-            return {
-                ...current,
-                [fieldName]: fieldValue,
-            }
-        })
-    }
+		setFieldValues((current) => {
+			return {
+				...current,
+				[fieldName]: fieldValue,
+			}
+		})
+	}
 
-    const formSubmitCompany = async (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        try {
-            const newCandidate = {
-                name: fieldValues.name,
-                email: fieldValues.email,
-                password: fieldValues.password,
-            }
+	const formSubmitCompany = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		try {
+			const newCandidate = {
+				name: fieldValues.name,
+				email: fieldValues.email,
+				password: fieldValues.password,
+				cpf: fieldValues.cpf,
+				phoneNumber: fieldValues.phoneNumber,
+			}
 
-            await makeServiceApi.post('/candidates', newCandidate)
+			const response = await makeServiceApi.post('/api/candidatos', newCandidate)
+			if (response.status === 201) {
+				toast.success('Usuário cadastrado com sucesso.')
+			}
+		} catch (err) {
+			console.error(err)
+		}
+	}
 
-        } catch (err) {
-            console.error(err)
-        }
-    }
+	return (
+		<div className="w-full h-screen flex items-center justify-center">
+			<ToastContainer />
+			<div className="sm:w-12/12 md:w-4/12 min-h-[80%] flex items-center justify-center flex-col bg-[#1a1a1e] p-5 rounded-md">
+				<h2 className="text-gray-200 font-bold text-3xl">Criar uma conta</h2>
+				<form onSubmit={formSubmitCompany} className="w-full flex flex-col gap-5 p-10">
+					<div className=" flex flex-col">
+						<label htmlFor="nomeEmpresa" className="w-30 font-medium text-gray-200 mb-1">Seu nome</label>
+						<input
+							type="text"
+							name="name"
+							id="name"
+							className="bg-[#121214] text-gray-200 p-3 rounded placeholder-slate-400 focus:outline-none focus:border-violet-800 focus:border-[1px] border-[1px] border-transparent"
+							placeholder="Nome e sobrenome"
+							value={fieldValues.name}
+							onChange={handleChangeValues}
+						/>
+					</div>
 
-    return (
-        <div className="w-full relative flex flex-col justify-center items-center">
-            <header className="w-full fixed bg-white top-0 flex items-center border-solid border-b-[2px] h-20 z-10">
-                <nav className="w-full flex items-center justify-end px-20">
-                    <Link to="/">
-                        <ArrowLeft size={48} color="#4f46e5" />
-                    </Link>
-                </nav>
-            </header>
+					<div className="flex flex-col">
+						<label htmlFor="" className="w-20 font-medium text-gray-200 mb-1">E-mail</label>
+						<input
+							type="text"
+							name="email"
+							className="bg-[#121214] text-gray-200 p-3 rounded placeholder-slate-400 focus:outline-none focus:border-violet-800 focus:border-[1px] border-[1px] border-transparent"
+							placeholder="E-mail"
+							value={fieldValues.email}
+							onChange={handleChangeValues}
+						/>
+					</div>
 
+					<div className="w-full flex flex-row gap-2 ">
+						<div className="w-full flex flex-col md:w-auto" >
+							<label htmlFor="" className="w-20 font-medium text-gray-200 mb-1">CPF</label>
+							<input
+								type="text"
+								name="cpf"
+								className="w-full bg-[#121214] text-gray-200 p-3 rounded placeholder-slate-400 focus:outline-none focus:border-violet-800 focus:border-[1px] border-[1px] border-transparent"
+								placeholder="cpf"
+								value={fieldValues.cpf}
+								onChange={handleChangeValues}
+							/>
+						</div>
 
-            <section className="w-7/12 p-5 rounded-md mt-5 ml-24">
-                <div className="w-full">
-                    <h1 className="my-10 text-violet-800 font-bold text-4xl ">
-                        Encontre seu melhor emprego aqui!
-                    </h1>
-                </div>
-                <div className="flex bg-white rounded-lg">
-                    <form className="w-full flex flex-col gap-5 p-10" onSubmit={formSubmitCompany}>
-                        <div className="flex flex-col">
-                            <input
-                                type="text"
-                                name="name"
-                                onChange={handleChangeValues}
-                                className="border-slate-200 bg-gray-100 p-3 rounded placeholder-slate-400 focus:outline-none focus:border-violet-800 border-[1px]"
-                                placeholder="Seu nome"
-                            />
-                        </div>
+						<div className="w-full flex flex-col md:w-auto" >
+							<label htmlFor="" className="w-20 font-medium text-gray-200 mb-1">Tel</label>
+							<input
+								type="text"
+								name="phoneNumber"
+								className="w-full bg-[#121214] text-gray-200 p-3 rounded placeholder-slate-400 focus:outline-none focus:border-violet-800 focus:border-[1px] border-[1px] border-transparent"
+								placeholder="Telefone"
+								value={fieldValues.phoneNumber}
+								onChange={handleChangeValues}
+							/>
+						</div>
+					</div>
 
-                        <div className="flex flex-row justify-between gap-5">
-                            <input
-                                type="text"
-                                name="email"
-                                onChange={handleChangeValues}
-                                className="flex-1 border-slate-200 bg-gray-100 p-3 rounded placeholder-slate-400 focus:outline-none focus:border-violet-800 border-[1px]"
-                                placeholder="Seu email"
-                            />
-                            <input
-                                type="password"
-                                name="password"
-                                onChange={handleChangeValues}
-                                className="border-slate-200 bg-gray-100 p-3 rounded placeholder-slate-400 focus:outline-none focus:border-violet-800 border-[1px]"
-                                placeholder="Sua senha"
-                            />
-                        </div>
+					<div className="flex flex-col">
+						<label htmlFor="" className="w-[1px] font-medium text-gray-200 mb-1">Senha</label>
+						<input
+							type="password"
+							name="password"
+							className=" bg-[#121214] text-gray-200 p-3 rounded placeholder-slate-400 focus:outline-none focus:border-violet-800 focus:border-[1px] border-[1px] border-transparent"
+							placeholder="Sua senha"
+							onChange={handleChangeValues}
+						/>
+					</div>
+					<div className="flex flex-col mt-3">
+						<button type="submit"
+							className="h-12 bg-violet-800 hover:bg-violet-900 text-white rounded py-3"
+						>
+							Cadastrar
+						</button>
 
-                        <div className="flex flex-col mt-3 ">
-                            <button type="submit" className="bg-indigo-800 hover:bg-indigo-900 transition text-white rounded py-3 w-60">
-                                CADASTRAR-SE
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </section>
-        </div>
-    )
+						<div className="mt-5">
+							<span className=" font-medium text-gray-200">
+								<a href="/"> Voltar para login</a>
+							</span>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	)
 }
