90615413fd0defbef509b79722523a73774580fa
Valdir Silva <valdirpiresba@gmail.com>
2025-01-22 22:02:36 -0300

    chore: configura module-alias para jest

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,4 +1,6 @@
+
 module.exports = {
+  roots: ['<rootDir>/src'],
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFiles: ['dotenv/config'],
@@ -6,4 +8,10 @@ module.exports = {
     '/node_modules/', // padrão padrão para ignorar node_modules
     './dist/', // adiciona esta linha para ignorar a pasta dist
   ],
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  }
 };
