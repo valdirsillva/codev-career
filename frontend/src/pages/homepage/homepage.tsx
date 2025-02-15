51f0276c3ecb169dea913536a6d703c196b94761
Valdir Silva <valdirpiresba@gmail.com>
2025-02-15 15:38:35 -0300

    feat: implementa listagem de vagas no perfil da empresa

--- frontend/src/pages/homepage/homepage.tsx
+++ frontend/src/pages/homepage/homepage.tsx
@@ -1,7 +1,7 @@
 import { Fragment } from "react"
 import { Header } from "../../components/header/header"
 import { useQueries } from "@tanstack/react-query"
-import { CircleDollarSign } from "lucide-react"
+import { CircleDollarSign, UserCheck } from "lucide-react"
 
 export function HomePage() {
 	const id = localStorage.getItem('@Auth:userId')
@@ -13,6 +13,17 @@ export function HomePage() {
 					const response = await fetch(`${process.env.REACT_APP_API}/api/vagas/${id}/empresa`)
 					if (!response.ok) {
 						throw new Error('Houve um erro ao tentar carregar seus dados')
+					}
+					return response.json()
+				},
+			},
+
+			{
+				queryKey: ['empresa'],
+				queryFn: async () => {
+					const response = await fetch(`http://localhost:9001/api/empresas/${id}`)
+					if (!response.ok) {
+						throw new Error('Erro na requisição');
 					}
 					return response.json()
 				},
@@ -30,20 +41,20 @@ export function HomePage() {
 	// Renderiza se houver um erro
 	if (isError) return <>Erro: {errors.join(', ')}</>
 
-	const [profile, profileData] = executeQueries.map((q) => q.data)
-
+	const [vacancies, profileData] = executeQueries.map((q) => q.data)
 	return (
 		<Fragment>
 			<Header data={{
 				label: 'Login',
 				routerPath: 'login'
 			}} />
-			<section className="w-full relative mt-24">
-				<div className="sm:w-full md:w-full md:flex md:flex-row sm:flex-col md:gap-5 px-3">
-					<section className="w-5/12 flex-col px-10 rounded-2xl ">
+			<section className="w-full mt-24">
+				<div className="sm:w-full md:w-full md:flex md:flex-row sm:flex-col md:gap-5 px-1">
+					{/* Quero que essa div fique a esquerda e fixa */}
+					<section className="w-[30rem] flex-col px-10 rounded-2xl fixed top-120 left-0 h-full">
 						<div className="flex flex-col items-center gap-3 p-8 mt-10 rounded-lg text-gray-200 bg-[#1a1a1e] ">
 							<div className="">
-								<h1 className="text-center text-2xl font-semibold text-indigo-600">Meu currículo pessoal</h1>
+								<h2 className="w-full flex justify-center font-bold tracking-wide text-lg">{profileData.name}</h2>
 							</div>
 							<div className="">
 								<img className="w-[150px] rounded-full"
@@ -52,33 +63,41 @@ export function HomePage() {
 								/>
 							</div>
 
+							<div className="w-full flex flex-col  gap-5">
+								<span className="text-center font-black line-clamp-3 tracking-wide">Setor: {profileData.sector}</span>
+								<span className="text-center line-clamp-3">{profileData.description}</span>
+							</div>
 						</div>
 					</section>
 
-					<section className="w-10/12 mb-5 mt-10  min-[200px] py-5 md:p-10 bg-[#1a1a1e] text-gray-200 border-2 border-transparent hover:border-2 hover:border-cyan-600 rounded-xl">
-						<div className="md:flex md:flex-col">
-							<span className="font-bold sm:text-3xl md:text-4xl text-indigo-600 ">
-								{profile[0].title}
-							</span>
+					{/* Quero que essa div fique a direiota e seja possivel rolar o scroll */}
+					<div className="ml-[30rem] w-full flex flex-row flex-wrap gap-3 mt-10 overflow-y-auto">
+						{vacancies.map((vacancy: any) => (
+							<section key={vacancy.id} className="w-[48%] mb-5 min-[200px] py-5 md:p-5 bg-[#1a1a1e] text-gray-200 border-2 border-transparent hover:border-2 hover:border-cyan-600 rounded-xl">
+								<div className="md:flex md:flex-col">
+									<span className="font-bold sm:text-3xl md:text-3xl text-indigo-600 ">
+										{vacancy.title}
+									</span>
 
-							<div className="flex flex-row items-center sm:gap-5 md:gap-10 mt-2 text-gray-200">
-								<span className="flex font-bold">
-									<CircleDollarSign color="#fff" className="sm:mr-0 md:mr-3" /> R$ {profile[0].salary}
-								</span>
-							</div>
+									<div className="flex flex-row items-center sm:gap-5 mt-2 text-gray-200">
+										<span className="flex font-bold">
+											<CircleDollarSign color="#fff" className="sm:mr-0 md:mr-3" /> R$ {vacancy.salary}
+										</span>
+									</div>
 
-							{/* <div className="flex flex-row items-center sm:gap-6 md:gap-10 mt-1 ">
-							</div>
+									<div className="sm:w-12/12 md:w-11/12 flex flex-col md:p-1 mt-5 text-justify">
+										{vacancy.description}
+									</div>
 
-							<div className="flex flex-row items-center gap-3 p-1 mt-1 ">
-								<Building2 size={20} />
-							</div>
+									<div className="sm:w-12/12 md:w-11/12 flex flex-row gap-2 items-center md:p-1 mt-5 ">
+										<span className="flex flex-row gap-2 items-center">Inscrições: 10  <UserCheck size={18} /></span>
 
-							<div className="sm:w-12/12 md:w-11/12 flex flex-col md:p-1 mt-10">
-								{profile[0].description}
-							</div> */}
-						</div>
-					</section>
+										<button>Candidatos</button>
+									</div>
+								</div>
+							</section>
+						))}
+					</div>
 				</div>
 			</section>
 		</Fragment>
